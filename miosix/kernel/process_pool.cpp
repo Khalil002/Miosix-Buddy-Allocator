c0277a4751c6d7e9f636e8e559121570bf9c5b60
Khalil El Hage Kassem <90007227+Khalil002@users.noreply.github.com>
2025-05-30 04:40:38 +0200

    checking allocate

--- miosix/kernel/process_pool.cpp
+++ miosix/kernel/process_pool.cpp
@@ -58,7 +58,7 @@ ProcessPool& ProcessPool::instance()
         reinterpret_cast<unsigned int>(&_process_pool_start));
     return pool;
     #else //TEST_ALLOC
-    static ProcessPool pool(reinterpret_cast<unsigned int*>(0x20008000),96*1024);
+    static ProcessPool pool(reinterpret_cast<unsigned int*>(0x20008000),1<<14);
     return pool;
     #endif //TEST_ALLOC
 }
