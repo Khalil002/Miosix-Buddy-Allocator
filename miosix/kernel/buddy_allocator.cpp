7f53da0271467677e5f8070654636a3c1a083d38
Khalil El Hage Kassem <90007227+Khalil002@users.noreply.github.com>
2025-06-01 06:14:48 +0200

    Fixed error where unusable block smaller than minimum block is allocated

--- miosix/kernel/buddy_allocator.cpp
+++ miosix/kernel/buddy_allocator.cpp
@@ -60,7 +60,7 @@ Buddy::Buddy(unsigned int *memBase, unsigned int memSize)
  * \param depthExp The current depth exponent in the buddy tree.
  */
 void Buddy::allocateUnusableBlock(Node *node, unsigned int targetExp, unsigned int depthExp){
-    if(depthExp == targetExp){
+    if(depthExp == targetExp || minBlockExp == targetExp){
         node->occupied = true; 
         node->unusable = true; 
         return;
