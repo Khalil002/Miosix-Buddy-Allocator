e2feaf91fb62cb0a2dd18feb130214b82cc62a14
Khalil El Hage Kassem <90007227+Khalil002@users.noreply.github.com>
2025-06-06 03:14:48 +0200

    Fixed allocateUnusableBlock function

--- miosix/kernel/buddy_allocator.cpp
+++ miosix/kernel/buddy_allocator.cpp
@@ -72,7 +72,6 @@ Buddy::Buddy(unsigned int *memBase, unsigned int memSize)
  */
 void Buddy::allocateUnusableBlock(Node *node, unsigned int targetExp, unsigned int depthExp){
     if(depthExp == targetExp || depthExp == minBlockExp){
-        node->occupied = true; 
         node->unusable = true; 
         return;
     }
