97f87992ecc4036b9d5c9944293d0d3b0f90410c
Khalil El Hage Kassem <90007227+Khalil002@users.noreply.github.com>
2025-06-22 04:58:03 +0200

    testing unusable block allocation

--- miosix/kernel/buddy_allocator.cpp
+++ miosix/kernel/buddy_allocator.cpp
@@ -390,7 +390,7 @@ void Buddy::allocateUnusableBlocksIterative(){
     unsigned int rightPtr = memPtr + (1 << (depth - 1));
     unsigned int maxPtr = memPtr + alignedSize;
 
-    while(rightPtr < maxPtr && depth > minBlockExp) {
+    while(rightPtr < maxPtr) {
         node->right = new Node();
         node = node->right;
         memPtr = rightPtr;
